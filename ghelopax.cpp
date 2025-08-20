a5b643e91589cc3968e0d4cec6688180e3e17693
Pham Gia Phu <giaphu899@gmail.com>
2025-08-20 17:20:27 +0700

    thêm lại base template, kh hiểu sao mất lol

---
+++ ghelopax.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ /\_/\  /\_/\  /\_/\  /\_/\
+( o.o )( o.o )( o.o )( o.o )
+ > ^ <  > ^ <  > ^ <  > ^ <
+ /\_/\                /\_/\
+( o.o )   ghelopax   ( o.o )
+ > ^ <                > ^ <
+ /\_/\  /\_/\  /\_/\  /\_/\
+( o.o )( o.o )( o.o )( o.o )
+ > ^ <  > ^ <  > ^ <  > ^ <
+*/
+
+#define ll long long
+#define ldb long double
+
+const int maxN = 1e5 + 5;
+const int MOD = 1e9 + 7;
+const int INF = 2e9;
+const ll INFLL = 4e18;
+
+#define el "\n"
+#define pb push_back
+#define eb emplace_back
+#define MASK(i) (1LL << (i))
+#define MID(l, r) ((l) + (((r) - (l)) >> 1))
+
+void input()
+{
+}
+
+void solve()
+{
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // freopen(".inp", "r", stdin);
+    // freopen(".out", "w", stdout);
+
+    input();
+    solve();
+
+    return 0;
+}
